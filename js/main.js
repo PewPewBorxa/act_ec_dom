c35b38f5c9a9184bff9529fefd83df9a3d60f5b1
botolo <datborx@gmail.com>
2020-10-26 09:01:28 +0100

    modificacion en main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.title = 'AE3_DOM_FORMULARIOS';
+h1 = document.createElement('h1');
+p1 = document.createElement('p');
+document.body.appendChild(p1);
+document.body.appendChild(h1);
+tb = document.createElement('textarea');
+
+
+
